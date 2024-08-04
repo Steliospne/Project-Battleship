95493d57ef7977bf3dfe1796c1d3b3ecd41fe018
Stelios Pnevmatikakis <63586679+Steliospne@users.noreply.github.com>
2024-08-04 20:54:01 +0200

    Briges the placeShip with the UI, When the user drag and drops a ship in the board, its location is calculated and stored in the object to then be returned so it can be used by the UI

--- src/controller.js
+++ src/controller.js
@@ -9,4 +9,15 @@ module.exports = class Controller {
     this.player_1 = new Player(player_1, new Gameboard());
     this.player_2 = new Player(player_2, new Gameboard());
   }
+
+  static handleShipPlacement(playerName, coordinates, ship) {
+    let currentPlayer = null;
+    let currentShip = null;
+    playerName === Controller.player_1.name
+      ? (currentPlayer = Controller.player_1)
+      : (currentPlayer = Controller.player_2);
+    currentShip = currentPlayer.gameboard.fleet[ship];
+    // currentPlayer.gameboard.placeShip(coordinates, currentShip);
+    return currentPlayer.gameboard.placeShip(coordinates, currentShip);
+  }
 };
